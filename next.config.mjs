1f444c21cab1a49eb431a5b4c7bfe615695de2e7
Just_A_Pony <pony@just-a-pony.net>
2025-03-09 02:53:35 +0800

    squash! First Commit
    First Commit

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,10 @@ import createNextIntlPlugin from 'next-intl/plugin';
 const withNextIntl = createNextIntlPlugin("./src/core/i18n/i18n.ts");
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  serverRuntimeConfig: {
+    runtime: process.env.RUNTIME
+  }
+};
 
 export default withNextIntl(nextConfig);
